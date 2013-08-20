c5432f898cb96ac3f6a65af9292d8fa11dd7b410
OrlandoLee <zongshengli@ucla.edu>
2013-08-20 04:28:53 +0000

    rails new youhou

---
+++ youhou/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Youhou::Application.config.secret_key_base = '3aaba38ab180e57bdef908b150603ee912af4ee90e1f910868f0b5b12c132bbd94da64bae25b7b142203bf445c95a3ec5144350e9533c1f5f2ef26f97ed09131'
