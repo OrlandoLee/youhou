b344a62b153068f381c3cc5de0dcf809303f5735
OrlandoLee <zongshengli@ucla.edu>
2013-08-26 21:41:45 -0700

    changing to psql --zl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Youhou::Application.config.secret_key_base = '7d369d78c779fe028c9339a85a24bdaf232ff65a8b03b10b1f26000c46e2f8b12eb865257a9cdabafe3ca1d4369ec5bfa45a7af5328f014c7cac2c0ac50d2b64'
