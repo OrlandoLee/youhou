ab907372ca1a9e3c6c38d13f7ee3d6d7f6137f37
OrlandoLee <zongshengli@ucla.edu>
2013-09-02 09:09:42 -0700

    nothing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "youhou.co@example.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.signup_confirmation.subject
-  #
   def signup_confirmation(user)
     @user = user
     @greeting = "Hi"
