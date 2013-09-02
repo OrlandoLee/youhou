d06ad577447ba36d37638368ac663d877424596a
OrlandoLee <zongshengli@ucla.edu>
2013-09-02 09:17:45 -0700

    adding sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,16 @@ Youhou::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+  }
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
