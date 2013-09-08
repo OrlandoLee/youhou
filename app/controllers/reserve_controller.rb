22ab30172e04efc349535f1f9ceb976064d39bdf
OrlandoLee <zongshengli@ucla.edu>
2013-09-08 10:02:30 -0700

    adding two frames for chaning bitcoins

--- app/controllers/reserve_controller.rb
+++ app/controllers/reserve_controller.rb
@@ -2,23 +2,9 @@ class ReserveController < ApplicationController
   def index
   end
   def new
-    @username = params[:username]
+    @amount = params[:amount]
+    @price = 100
+    @commit = params[:commit]
     #add judgement
-  end
-  def save
-    @user = User.new
-    if !User.find_by_username(params[:username]) && !User.find_by_email(params[:email]) #not safe
-     @user.username = params[:username]
-     @user.email = params[:email]
-     if @user.save
-       UserMailer.signup_confirmation(@user).deliver
-     else
-      #raise 'can not send email'
-       logger.info("can not send email for #{@user.username}")
-     end
-    else
-      logger.info("username: #{@user.username} or email exists")
-      #raise 'username or email exists'
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Youhou::Application.routes.draw do
 
   # Example of regular route:
   get 'reserve' => 'reserve#new'
-  post 'reserve' => 'reserve#save'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
