a13adcdb7a32893d1a102f79d66e993195cd8e9a
OrlandoLee <zongshengli@ucla.edu>
2013-09-08 12:34:56 -0700

    fixing index problem

--- app/controllers/reserve_controller.rb
+++ app/controllers/reserve_controller.rb
@@ -8,7 +8,7 @@ class ReserveController < ApplicationController
     @orderbook_json = JSON.parse(@orderbook)
     if @orderbook_json["result"] == "success"
       @asks = JSON.parse(@orderbook)["return"]["asks"]
-      @bids = JSON.parse(@orderbook)["return"]["bids"]#.reverse
+      @bids = JSON.parse(@orderbook)["return"]["bids"].reverse
     else
       raise "can not get data from url"
     end
@@ -16,50 +16,50 @@ class ReserveController < ApplicationController
     @amount = params[:amount]
     @commit = params[:commit]
     if @commit == "Sell"
-      @index = 0
-      size_of_bids = @bids.size
-      @sell_amount_left = @amount.to_f
-      @result = 0.0
-      while (@sell_amount_left > 0 && @index < size_of_bids)
-        if @sell_amount_left >= @bids[@index]["amount"]
-          @sell_amount_left = @sell_amount_left - @bids[@index]["amount"]
-          @index = @index +1
-          @result = @result + (@bids[@index]["price"]*@bids[@index]["amount"])
-        else
-           @result = @result + (@bids[@index]["price"]*@sell_amount_left)
-           @sell_amount_left = 0
-        end
-      end
-      if @sell_amount_left == 0
-        @result = @result + (@result * 0.01)
-      else
-        raise "amount is too large"
-      end
+          index = 0
+          size_of_bids = @bids.size
+          sell_amount_left = @amount.to_f
+          @result = 0.0
+          while ((sell_amount_left > 0) && (index < size_of_bids)) do
+            if (sell_amount_left >= @bids[index]["amount"])
+              sell_amount_left = sell_amount_left - @bids[index]["amount"]
+              @result = @result + (@bids[index]["price"]*@bids[index]["amount"])
+              index = index +1
+            else
+               @result = @result + (@bids[index]["price"]*sell_amount_left)
+               sell_amount_left = 0
+            end
+          end
+          if sell_amount_left == 0
+            @result = @result + (@result * 0.01)
+          else
+            raise "amount is too large"
+          end          
     elsif @commit == "Buy"
-      @index = 0
-      size_of_asks = @asks.size
-      @buy_amount_left = @amount.to_f
-      @result = 0.0
-      
-      while (@buy_amount_left > 0 && @index < size_of_asks)
-              if @buy_amount_left >= @asks[@index]["amount"]
-                @buy_amount_left = @buy_amount_left.to_f - @asks[@index]["amount"].to_f
-                @index = @index +1
-                @result = @result + (@asks[@index]["price"].to_f*@asks[@index]["amount"].to_f)
-              else
-                 @result = @result + (@asks[@index]["price"].to_f*@buy_amount_left.to_f)
-                 @buy_amount_left = 0
-              end
-      end
-      
-      if @buy_amount_left == 0
-        @result = @result + (@result * 0.01)
-      else
-        raise "amount is too large"
-      end
-    else
-      raise 'wrong option'
-    end
+          index = 0
+          size_of_asks = @asks.size
+          buy_amount_left = @amount.to_f
+          @result = 0.0
+
+          while ((buy_amount_left > 0) && (index < size_of_asks))
+                  if (buy_amount_left >= @asks[index]["amount"])
+                    buy_amount_left = buy_amount_left.to_f - @asks[index]["amount"].to_f
+                    @result = @result + (@asks[index]["price"].to_f*@asks[index]["amount"].to_f)
+                    index = index +1
+                  else
+                     @result = @result + (@asks[index]["price"].to_f*buy_amount_left.to_f)
+                     buy_amount_left = 0
+                  end
+          end
+
+          if buy_amount_left == 0
+            @result = @result + (@result * 0.01)
+          else
+            raise "amount is too large"
+          end
+        else
+          raise 'wrong option'
+        end
     #add judgement
   end
 end
