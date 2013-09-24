b766c982b022e561fe464a390787d28749218b73
OrlandoLee <zongshengli@ucla.edu>
2013-09-23 17:11:17 -0700

    fixing video

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,6 @@
 //= require jquery.easing-1.3.min.a5cbacf
 //= require jquery-1.8.3.min.e128811
 //= require vFX4fOJujEGQue0oxfvaKA.f5384bc
-//below things must be put in the behind
 //= require bigvideo/jquery-1.7.2.min
 //= require bigvideo/jquery-ui-1.8.22.custom.min
 //= require bigvideo/jquery.imagesloaded.min
